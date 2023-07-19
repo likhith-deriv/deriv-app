f90050952b5ed2c6c3349140214e76c4ec2329f1
Likhith Kolayari <likhith@regentmarkets.com>
2023-07-19 15:02:29 +0400

    test: :white_check_mark: added testcase

--- packages/account/src/Components/financial-details/__tests__/financial-details.spec.tsx
+++ packages/account/src/Components/financial-details/__tests__/financial-details.spec.tsx
@@ -3,6 +3,7 @@ import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import { isDesktop, isMobile } from '@deriv/shared';
 import FinancialDetails, { TFinancialDetails } from '../financial-details';
 import { FormikValues } from 'formik';
+import userEvent from '@testing-library/user-event';
 
 jest.mock('@deriv/shared', () => ({
     ...jest.requireActual('@deriv/shared'),
@@ -14,44 +15,8 @@ const modal_root_el = document.createElement('div');
 modal_root_el.setAttribute('id', 'modal_root');
 document.body.appendChild(modal_root_el);
 
-const fields_enums = {
-    account_turnover_enum: [
-        { value: 'account turnover 1', text: 'account turnover 1' },
-        { value: 'account turnover 2', text: 'account turnover 2' },
-    ],
-    education_level_enum: [
-        { value: 'education level 1', text: 'education level 1' },
-        { value: 'education level 2', text: 'education level 2' },
-    ],
-    employment_industry_enum: [
-        { value: 'employment industry 1', text: 'employment industry 1' },
-        { value: 'employment industry 2', text: 'employment industry 2' },
-    ],
-    estimated_worth_enum: [
-        { value: 'estimated worth 1', text: 'estimated worth 1' },
-        { value: 'estimated worth 2', text: 'estimated worth 2' },
-    ],
-    income_source_enum: [
-        { value: 'income source 1', text: 'income source 1' },
-        { value: 'income source 2', text: 'income source 2' },
-    ],
-    net_income_enum: [
-        { value: 'net income 1', text: 'net income 1' },
-        { value: 'net income 2', text: 'net income 2' },
-    ],
-    occupation_enum: [
-        { value: 'occupation 1', text: 'occupation 1' },
-        { value: 'occupation 2', text: 'occupation 2' },
-    ],
-
-    source_of_wealth_enum: [
-        { value: 'source of wealth 1', text: 'source of wealth 1' },
-        { value: 'source of wealth 2', text: 'source of wealth 2' },
-    ],
-};
-
 describe('<FinancialDetails />', () => {
-    let mock_props: TFinancialDetails = {
+    const mock_props: TFinancialDetails = {
         getCurrentStep: jest.fn(),
         goToNextStep: jest.fn(),
         onCancel: jest.fn(),
@@ -61,13 +26,6 @@ describe('<FinancialDetails />', () => {
         goToPreviousStep: jest.fn(() => ({ errors: {} })),
         value: {},
     };
-
-    beforeEach(() => {
-        mock_props = {
-            ...mock_props,
-            ...fields_enums,
-        };
-    });
 
     const fieldsRenderCheck = () => {
         expect(screen.getByText('Anticipated annual turnover')).toBeInTheDocument();
@@ -163,5 +121,20 @@ describe('<FinancialDetails />', () => {
         await waitFor(() => {
             expect(mock_props.onSubmit).toHaveBeenCalledTimes(1);
         });
+    });
+
+    it('should change the selected value when user changes the value in the dropdown', () => {
+        (isDesktop as jest.Mock).mockReturnValue(false);
+        (isMobile as jest.Mock).mockReturnValue(true);
+
+        render(<FinancialDetails {...mock_props} />);
+
+        const select_inputs = screen.getAllByRole('combobox');
+
+        const income_source_select = select_inputs.find((option: FormikValues) => option.name === 'income_source');
+
+        userEvent.selectOptions(income_source_select as HTMLElement, 'Salaried Employee');
+
+        expect(screen.getByRole('option', { name: 'Salaried Employee' }).selected).toBe(false);
     });
 });
