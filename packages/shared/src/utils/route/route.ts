9efe2414c0486771e3dd7deba21efb2aefaaa0ad
Likhith Kolayari <likhith@regentmarkets.com>
2023-08-28 15:08:37 +0400

    fix: :art: refactored code

--- packages/shared/src/utils/route/route.ts
+++ packages/shared/src/utils/route/route.ts
@@ -5,7 +5,7 @@ export type TRoute = {
     component?: React.ElementType | null;
     default?: boolean;
     exact?: boolean;
-    getTitle: () => string;
+    getTitle?: () => string;
     icon_component?: string;
     id?: string;
     is_authenticated?: boolean;
@@ -19,7 +19,8 @@ type TGetSelectedRoute = {
     pathname: string;
 };
 
-export const matchRoute = (route: TRoute, pathname: string) => new RegExp(`^${route.path}(/.*)?$`).test(pathname);
+// @ts-expect-error as this is a utility function with dynamic types
+export const matchRoute = <T>(route: T, pathname: string) => new RegExp(`^${route?.path}(/.*)?$`).test(pathname);
 
 export const getSelectedRoute = ({ routes, pathname }: TGetSelectedRoute) => {
     const matching_route = routes.find(route => matchRoute(route, pathname));
